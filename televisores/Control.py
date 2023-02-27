983401f6aeb4f3660f7a7db0b35a46d230c6daff
lvivasd <lvivasd@unal.edu.co>
2023-02-27 00:03:05 -0500

    Intento 1

---
+++ televisores/Control.py
@@ -0,0 +1,28 @@
+from TV import TV
+class Control:
+    def __init__(self, tv):
+        self._tv = tv
+    
+    def turnOn(self):
+        TV.turnOn()
+    def turnOff(self):
+        TV.turnOff()
+    def canalUp(self):
+        TV.canalUp()
+    def canalDown(self):
+        TV.canalDown()
+    def volumenUp(self):
+        TV.volumenUp()
+    def volumenDown(self):
+        TV.volumenDown()
+    def setCanal(self):
+        TV.setCanal()
+
+    def enlazar(self, tv):
+        self._tv = tv
+        TV.setControl(self)
+    
+    def getTv(self):
+        return self._tv
+    def setTv(self, tv):
+        self._tv = tv
